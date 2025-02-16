b36ed113c805e2610058f18eccab9341bd3eb196
sivanargana <siva.nargana66@gmail.com>
2025-02-16 18:25:28 +0530

    new changes

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import path from 'path';
+import puppeteer from "puppeteer"; 
 const viewsPath = new URL('./views', import.meta.url).pathname;
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', viewsPath); 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+
+await page.goto('https://developer.chrome.com/');
   res.render('index');
 });
 
